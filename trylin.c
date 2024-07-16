f03c050d5e59ba834b18d0510a2b9b81f942849e
Ofek Cohen <ofekcohen977@gmail.com>
2024-07-16 14:08:20 +0300

    linux test

--- trylin.c
+++ trylin.c
@@ -2,7 +2,7 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-sem_t sem1, sem2, sem3;
+sem_t sem1, sem2, sem3;   // sem2=2 the rest 0
 
 void* f1(void* p) {
     while(1) {
@@ -25,20 +25,21 @@ void* f3(void* p) {
     while(1) {
         sem_wait(&sem3);
         printf("C");
-        sem_post(&sem1); 
-        sem_post(&sem1); 
+        sem_post(&sem1);  
+        sem_post(&sem1);  
     }
 }
 
 int main() {
     pthread_t t1, t2, t3;
-
-    sem_init(&sem1, 0, 2); 
-    sem_init(&sem2, 0, 0); 
+ 
+    sem_init(&sem1, 0, 4); 
+    sem_init(&sem2, 0, 1); 
     sem_init(&sem3, 0, 0); 
-
+    
     pthread_create(&t1, NULL, f1, NULL);
     pthread_create(&t2, NULL, f2, NULL);
     pthread_create(&t3, NULL, f3, NULL);
     return 0;
 }
+

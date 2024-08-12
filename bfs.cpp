223a39201fedb809a493b1eeaf5294c29f3e0a1f
Ofek <ofekcohen977@gmail.com>
2024-08-12 11:49:46 +0300

    new

---
+++ bfs.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <queue> // for bfs later
+#include <set>
+using namespace std;
+
+
+void bfs(string start, string end, map<string, vector<string>>& graph, map<pair<string, string>, double>& w) {
+    if (graph.find(start) == graph.end() || graph.find(end) == graph.end()) {
+        cout << "0"; // Start or end node not in the graph
+        return;
+    }
+    if (start == end) {
+        cout << "1"; // Start and end are the same
+        return;
+    }
+
+    map<string, string> parent;  // Save the "parent" of each node
+    queue<string> q;
+    set<string> visited;
+
+    q.push(start);
+    visited.insert(start);
+
+    while (!q.empty()) {
+        string t = q.front();
+        q.pop();
+
+        if (t == end) {
+            cout << "1"; // Path found
+            return;
+        }
+
+        for (const auto& neighbor : graph[t]) {
+            if (visited.find(neighbor) == visited.end()) { // If neighbor has not been visited
+                visited.insert(neighbor);
+                q.push(neighbor);
+            }
+        }
+    }
+    cout << "0"; // No path found
+}
+
+
+void createGraph(vector<vector<string>>& edges, vector<double>& weights) 
+{
+    map< string , vector<string >> graph;   // graph(node a)=[b,c,d]  vector of neighbors 
+    map< pair<string,string> , double > w; // w(<a,b>)=5
+    
+    // create the graph
+    for(int i=0; i<weights.size();i++)
+    {
+        // add nodes and edges
+        graph[edges[i][0]].push_back(edges[i][1]);   // graph[a]=[b]
+
+        // add weights to edges
+        w[{edges[i][0],edges[i][1]}]= weights[i];   // w[{a,b}]=2
+    }
+
+    bfs("a", "c",graph,w);
+}
+
+
+int main() 
+{
+    vector<vector<string>> edges = {{"a","b"}, {"b","c"}};
+    vector<double> weights = {2.0, 3.0};  
+    
+    createGraph(edges, weights);   
+    return 0;
+}

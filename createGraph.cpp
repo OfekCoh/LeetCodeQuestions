6cbffeadb861d746bda3cce9c5ec95724f528a08
Ofek <ofekcohen977@gmail.com>
2024-08-12 11:38:17 +0300

    new

--- createGraph.cpp
+++ createGraph.cpp
@@ -13,12 +13,10 @@ void createGraph(vector<vector<string>>& edges, vector<double>& weights)
     for(int i=0; i<weights.size();i++)
     {
         // add nodes and edges
-        graph[edges[i][0]].push_back(edges[i][1]);  
-        graph[edges[i][1]].push_back(edges[i][0]);  
+        graph[edges[i][0]].push_back(edges[i][1]);   // graph[a]=[b]
 
         // add weights to edges
-        w[{edges[i][0],edges[i][1]}]= weights[i];
-        w[{edges[i][1],edges[i][0]}]= 1.0/weights[i];
+        w[{edges[i][0],edges[i][1]}]= weights[i];   // w[{a,b}]=2
     }
 }
 

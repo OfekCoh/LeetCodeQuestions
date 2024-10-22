99b3ab10f3e0964fa91cd7fcea6cac4ae2675761
Ofek <ofekcohen977@gmail.com>
2024-10-22 20:26:25 +0300

    new

--- minEatingSpeed.cpp
+++ minEatingSpeed.cpp
@@ -1,16 +1,21 @@
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int low=1, high=*max_element(piles.begin(), piles.end()), mid;
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
 
-        while(low<=high)
-        {
-            mid=(low+high)/2;
-            long long sum=0;
-            for(int num: piles) {
-                sum+= num/mid;
-                if(num%mid) sum++; // if we get a fraction need to add another whole hour 
-            }
-            if(sum>h) low=mid+1;
-            else high=mid-1;
+int minEatingSpeed(vector<int>& piles, int h) {
+    int low=1, high=*max_element(piles.begin(), piles.end()), mid;
+
+    while(low<=high)
+    {
+        mid=(low+high)/2;
+        long long sum=0;
+        for(int num: piles) {
+            sum+= num/mid;
+            if(num%mid) sum++; // if we get a fraction need to add another whole hour 
         }
-        return low;
+        if(sum>h) low=mid+1;
+        else high=mid-1;
     }
+    return low;
+}

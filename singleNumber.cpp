e7ef680a15e16a2f89fc195cfca7e3591951d4c4
Ofek <ofekcohen977@gmail.com>
2024-12-07 13:20:05 +0200

    new

--- singleNumber.cpp
+++ singleNumber.cpp
@@ -1,3 +1,6 @@
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -6,7 +9,7 @@ int singleNumber(vector<int>& nums)
 {
     int n=nums.size(),temp=nums[0];
     for(int i=0;i<n-1;i++){
-        temp= temp^nums[i+1];
+        temp= temp^nums[i+1];  // using XOR between all elements. A^A=0, 0^B=B.
     }
     return temp;
 }

32b79ab924d26e50895528d8c18ed5a1cc2d877e
Ofek <ofekcohen977@gmail.com>
2025-02-17 15:00:22 +0200

    ץ

---
+++ mergeIntervals.cpp
@@ -0,0 +1,26 @@
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    vector<vector<int>> res;
+    vector<pair<int,int>> temp;
+
+    for(auto pair : intervals) temp.push_back({pair[0], pair[1]});
+    sort(temp.begin(), temp.end());
+    
+    int first, second;
+
+    for(int i=0;i<temp.size();i++) {
+        first=temp[i].first;
+        second=temp[i].second;
+        vector<int> curr;
+        
+        while(i<temp.size()-1 && temp[i+1].first <= second) {
+            second= max(second, temp[i+1].second);
+            i++;
+        }
+        
+        curr.push_back(first);
+        curr.push_back(second);
+        res.push_back(curr);
+    }
+    
+    return res;
+}
